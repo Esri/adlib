58a94e598e3cd4e3680143f7a4a568fa31a248bc
Dave Bouwman <dave@davebouwman.com>
2017-09-19 08:48:29 -0600

    updated doc versions and remove logging

--- docs/js/adlib.umd.js
+++ docs/js/adlib.umd.js
@@ -1,5 +1,5 @@
 /**
-* adlib - v0.2.0 - Tue Sep 05 2017 14:35:20 GMT-0600 (MDT)
+* adlib - v0.2.0 - Mon Sep 18 2017 16:01:09 GMT-0600 (MDT)
 * Copyright (c) 2017 Dave Bouwman / Esri
 * Apache-2.0
 */
@@ -6294,10 +6294,196 @@ function deepMapValues(object, callback, propertyPath) {
 }
 
 /**
+ * The arborist is responsible for pruning trees with nodes/entries
+ * marked with `{{delete:NNN}}`
+ *
+ * There are multiple exports, mainly to allow for easy testing of the
+ * worker functions. Only `arborist` is meant to be used directy
+ */
+function isObject$3 (v) {
+  return typeof v === 'object'
+}
+
+/**
+ * Trim a tree decorated with `{{delete:NNN}}`
+ */
+function arborist (object, propertyPath) {
+  propertyPath = propertyPath || '';
+
+  if(Array.isArray(object)){
+
+    var arrResults = object.map(iteratee).filter(function (e) {
+      return e;  // remove any nulls
+    });
+    return pruneArray(arrResults);
+
+  } if(object && isObject$3(object) ) {
+
+    return pruneObject(lodash_mapvalues(object, iteratee));
+
+  } else {
+
+    return getPropertyValue(object, propertyPath);
+  }
+
+  function iteratee(value, key){
+    var valuePath = propertyPath ? propertyPath + '.' + key: key;
+    return arborist(value, valuePath);
+  }
+}
+
+/**
+ * Prune an array
+ * For all the entries in the array...
+ *    if the entry is a naked string and contains `{{delete:NNN}}`
+ *      get maximum NNN value
+ *    then
+ *      if maxN === 0
+ *        return an empty array
+ *      if maxN > 0
+ *        return `{{delete:maxN-1}}`
+ *    else
+ *      return array
+ */
+function pruneArray (arrResults) {
+  var res = arrResults;
+  // is any entry a string w/ {{delete}}?
+  var maxLevel = arrResults.reduce(function (maxLevel, e) {
+    if (isString$1(e) && hasDelete(e)) {
+      var lvl = getLevel(e);
+      if (lvl > maxLevel) {
+        maxLevel = lvl;
+      }
+    }
+    return maxLevel;
+  }, -1);
+
+  if (maxLevel > -1) {
+    if (maxLevel === 0) {
+      res = [];
+    } else {
+      res = "{{delete:" + (maxLevel - 1) + "}}";
+    }
+  }
+
+  return res;
+}
+
+
+function pruneObject (objResults) {
+  // console.log(`   pruneObject:: working on ${JSON.stringify(objResults)}`);
+  var startVal = {obj: {}, maxLevel: -1 };
+  var res;
+  // cook a new clone object, and track the maxLevel
+  var reduction = Object.keys(objResults).reduce(function (acc, key) {
+    var val = objResults[key];
+    if (isString$1(val) && hasDelete(val)) {
+      var lvl = getLevel(val);
+      if (lvl > acc.maxLevel) {
+        acc.maxLevel = lvl;
+      }
+    } else {
+      // only add the prop if it's not a `{{delete:NNN}}`
+      acc.obj[key] = val;
+    }
+    return acc;
+  }, startVal);
+  // if -1, we return entire object...
+  // if 0 we just remove the prop...
+  // if 1 we return undefined...
+  // if > 1 we return the deleteVal
+  if (reduction.maxLevel > 0 ) {
+    if (reduction.maxLevel === 1) {
+      res = undefined;
+    } else {
+      res = "{{delete:" + (reduction.maxLevel - 1) + "}}";
+    }
+  } else {
+    res = reduction.obj;
+  }
+
+  // console.log(`     returning ${JSON.stringify(res)}`);
+  return res;
+}
+
+/**
+ * Get a value for a property, handling the `{{delete:NNN}}` syntax
+ */
+function getPropertyValue (val){
+  var output = val;
+
+  if (typeof val === 'string') {
+    if (hasDelete(val)) {
+      output = getDeleteValue(val);
+    }
+  }
+  return output;
+}
+
+/**
+ * Given a string with `{{delete:NNN}}`
+ * if NNN === 0 return undefined
+ * else return `{{delete:NNN - 1}}`
+ */
+function getDeleteValue (val) {
+  var output = val;
+  var level = getLevel(val);
+  if (level === 0) {
+    output = undefined;
+  } else {
+    output = "{{delete:" + level + "}}";
+  }
+  return output;
+}
+
+/**
+ * Extract the level from a `{{delete:NNN}}`
+ */
+function getLevel (value) {
+  return parseInt(value.replace(/{|}/g, '').split(':')[1]);
+}
+
+/**
+ * Simple check if a value has `{{delete` in it
+ */
+function hasDelete (value) {
+  if (value && typeof value === 'string') {
+    return value.indexOf('{{delete') > -1;
+  } else {
+    return false;
+  }
+}
+
+function isString$1 (v) {
+  return typeof v === 'string';
+}
+
+/**
+ * Optional Transform
+ * Supports a declarative syntax for optional template properties
+ *
+ * {{some.object.key:optional:2}}
+ *
+ * In this example, if defined, the value of `some.object.key` is used.
+ * If not defined, then the optional transform is utilized
+ * and a post-processing step is executed which will remove two parent levels
+ * from the output structure
+ */
+
+function optional(key, value, settings, level) {
+  if ( level === void 0 ) level = 0;
+
+  // console.log(`optional: ${key}, ${value}, ${level}`);
+  var val = value;
+  if (!value) {
+    val = "{{delete:" + level + "}}";
+  }
+  return val;
+}
+
+/**
  * adlib.js
  */
-//import Transforms from './transforms'
-
 function isString(v) {
   return typeof v === 'string';
 }
@@ -6314,17 +6500,17 @@ function _swap(parameter, settings, transforms) {
       var key = transformCheck[0];
       var fn = transformCheck[1];
       // we default to using the value...
-      var type = 'value';
+      var param = null;
       if (transformCheck[2]){
-        type = transformCheck[2];
+        param = transformCheck[2];
       }
       if(transforms && transforms[fn] && typeof transforms[fn] === 'function') {
         // get the value from the param
         value = lodash_get(settings, key);
         // transform it...
-        value = transforms[fn](key, value, settings);
+        value = transforms[fn](key, value, settings, param);
       } else {
-        throw new Error(("Attempted to apply non-existant transform " + fn + " on " + type + " with params " + key));
+        throw new Error(("Attempted to apply non-existant transform " + fn + " on " + key + " with params " + param));
       }
     } else {
       // we just get the value
@@ -6335,9 +6521,16 @@ function _swap(parameter, settings, transforms) {
 
 // Combine a Template with Settings
 function adlib(template, settings, transforms) {
-    if ( transforms === void 0 ) transforms = null;
+  if ( transforms === void 0 ) transforms = null;
 
-    return deepMapValues(template, function(templateValue, templatePath){
+  transforms = transforms || {};
+  if (transforms.optional) {
+    throw new Error('Please do not pass in an `optional` transform, adlib provides that interally.');
+  } else {
+    transforms.optional = optional;
+  }
+
+    var res = deepMapValues(template, function(templateValue, templatePath){
         // Only string templates
         if (!isString(templateValue)) {
           return templateValue;
@@ -6378,6 +6571,7 @@ function adlib(template, settings, transforms) {
           return templateValue;
         }
     });
+    return arborist(res);
 }
 
 return adlib;
